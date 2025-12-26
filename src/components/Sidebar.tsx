05a4abbe0f48674a846ce1150844d3b749355d8c
iwyang <455343442@qq.com>
2025-12-26 09:38:56 +0800

    Update Sidebar.tsx

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -131,11 +131,6 @@ const Sidebar = ({ onToggle, activePath = '/' }: SidebarProps) => {
 
   const [menuItems, setMenuItems] = useState([
     {
-      icon: Globe,
-      label: '源浏览器',
-      href: '/source-browser',
-    },
-    {
       icon: Film,
       label: '电影',
       href: '/douban?type=movie',
